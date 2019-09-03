8c5228ec415d347edc3bb9194fca8587a0d7d3fd
waspryo <wasp2123321@gmail.com>
2019-09-04 00:13:39 +0900

    add testing

--- httpd/main.go
+++ httpd/main.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"newfeeder/httpd/platform/newsfeed"
-)
-
 func main() {
 	// r := gin.Default()
 
@@ -12,11 +7,4 @@ func main() {
 
 	// r.Run()
 
-	feed := newsfeed.New()
-
-	fmt.Println(feed)
-
-	feed.Add(newsfeed.Item{"Hello", "How ya' doing mate?"})
-
-	fmt.Println(feed)
 }
---
+++ httpd/platform/newsfeed/newsfeed_test.go
@@ -0,0 +1,9 @@
+package newsfeed
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+
+}
+
+func TestGetAll(t *testing.T) {}
